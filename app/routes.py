cdc8dff2a77fe4efa8984dd958bbfc781c051d15
mariatrapicyna <trmasha2015@yandex.ru>
2020-01-25 14:28:17 +0300

    add files

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,14 @@
 from flask import render_template
 from app import app
-
-
+from app.forms import CodeForm
+from flask import render_template, flash, redirect
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('base.html')
+    form = CodeForm()
+    if form.validate_on_submit():
+        f = open("file.txt", "w")
+        f.write(form.code.data)
+        return redirect('/index')
+    return render_template('base.html', title="Code In", form=form)
+
---
+++ check.py
@@ -0,0 +1,2 @@
+import os
+print(os.system("pycodestyle --statistics runner.py"))
