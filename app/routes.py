d5cbdd6374e1826898ffd3379be8eafa5342b6b5
mariatrapicyna <trmasha2015@yandex.ru>
2020-01-26 14:54:59 +0300

    add levels

--- app/routes.py
+++ app/routes.py
@@ -33,3 +33,33 @@ def check():
     pipe = os.popen(command)
     result = pipe.readlines()
     return jsonify({'result': result})
+
+
+@app.route('/one', methods=['GET', 'POST'])
+def one():
+    form = CodeForm()
+    return render_template('game1.html', form=form)
+
+
+
+@app.route('/two', methods=['GET', 'POST'])
+def two():
+    form = CodeForm()
+    return render_template('game2.html', form=form)
+
+
+@app.route('/three', methods=['GET', 'POST'])
+def three():
+    form = CodeForm()
+    return render_template('game3.html', form=form)
+
+
+@app.route('/four', methods=['GET', 'POST'])
+def four():
+    form = CodeForm()
+    return render_template('game4.html', form=form)
+
+@app.route('/five', methods=['GET', 'POST'])
+def five():
+    form = CodeForm()
+    return render_template('game5.html', form=form)
--- file.py
+++ file.py
@@ -1,2 +1,20 @@
-a = 1
+def FUNC(a=0, c=2):
+    print(a)
 
+
+def FUNC2(a=1, c=2):
+    print(c)
+
+
+FUNC()
+FUNC2()
+max_n = 100
+left = 0
+right = 100
+while(right - left > 1):
+    m = ((left + right) // 2)
+    if (m <= x):
+        left = m
+    else:
+        right = m
+print(left)
